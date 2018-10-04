c64496c24e2a6d4cd0a17c29c76388debbea6226
spreadappjl <spreadapp.jl@gmail.com>
2018-10-04 19:42:24 -0300

    main

--- lib/components/post.dart
+++ lib/components/post.dart
@@ -4,7 +4,7 @@ class Post extends StatelessWidget {
 
   String title;
 
-  Post(title) {
+  Post(title, document) {
     this.title = title;
   }
   
@@ -12,8 +12,8 @@ class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Theme.of(context).primaryColor.withOpacity(0.9),
-      margin: const EdgeInsets.only(left: 6.0, right: 6.0, bottom: 12.0, top: 10.0),
+      color: Colors.black12.withOpacity(0.44),
+      margin: const EdgeInsets.only(left: 6.0, right: 6.0, bottom: 12.0, top: 0.0),
 
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -22,7 +22,7 @@ class Post extends StatelessWidget {
             children:<Widget>[
               //Perfil Circle Image
               Container(
-                margin: EdgeInsets.only(top: 5.0, left: 4.0, right: 10.0, bottom: 5.0),
+                margin: EdgeInsets.only(top: 6.0, left: 4.0, right: 10.0, bottom: 5.0),
                 width: 21.0,
                 height: 21.0,
                 decoration: BoxDecoration(
@@ -38,7 +38,7 @@ class Post extends StatelessWidget {
                 child: Text(
                   title.toUpperCase(),
                   style: TextStyle(
-                    fontSize: 11.0,
+                    fontSize: 13.0,
                     fontWeight: FontWeight.normal
                   ),
                 ),
@@ -50,7 +50,7 @@ class Post extends StatelessWidget {
             margin: EdgeInsets.only(left: 2.0, right: 2.0, top: 1.0,bottom: 7.0),
             color: Color.fromRGBO(200,200,200,.5),
             width: double.infinity,
-            height: 185.0,
+            height: 181.0,
             child: Image.network(
               'http://www.afortunadas.com.br/wp-content/uploads/2017/06/festival_de_fries_ribeirao_preto_1200x635px_0-1160x635.jpg',
               fit: BoxFit.fill,
@@ -66,21 +66,21 @@ class Post extends StatelessWidget {
                   children: <Widget>[        
                     Expanded(
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,          
+                        mainAxisAlignment: MainAxisAlignment.start,          
                         children: <Widget> [
                           Container(
                             //padding: EdgeInsets.only(left: 0.8),
-                                padding: EdgeInsets.only(right: 11.0,left: 0.0,top: 0.0, bottom: 0.0),
-                                child: Icon(Icons.favorite_border, size: 21.0, color: Theme.of(context).iconTheme.color.withOpacity(0.7)),
+                                padding: EdgeInsets.only(right: 10.0,left: 7.0,top: 5.0, bottom: 20.0),
+                                child: Icon(Icons.more_vert, size: 23.0, color: Theme.of(context).iconTheme.color.withOpacity(0.7)),
                                 ),
                                 Container(
                             //padding: EdgeInsets.only(left: 0.8),
-                                padding: EdgeInsets.only(right: 10.0,left: 0.0,top: 0.0, bottom: 0.0),
-                                child: Icon(Icons.share, size: 21.0, color: Theme.of(context).iconTheme.color.withOpacity(0.7)),
+                                padding: EdgeInsets.only(right: 17.0,left: 0.0,top: 5.0, bottom: 20.0),
+                                child: Icon(Icons.share, size: 23.0, color: Theme.of(context).iconTheme.color.withOpacity(0.7)),
                                 ),
                                 Container(
-                            //padding: EdgeInsets.only(right: 0.0, left: 8.0, top: 5.0, bottom: 10.0),
-                                child: Icon(Icons.more_vert, size: 21.0, color: Theme.of(context).iconTheme.color.withOpacity(0.7)),
+                                padding: EdgeInsets.only(right: 0.0, left: 0.0, top: 5.0, bottom: 22.0),
+                                child: Icon(Icons.favorite_border, size: 23.0, color: Theme.of(context).iconTheme.color.withOpacity(0.7)),
                                 ),
                               ],
                             ),
