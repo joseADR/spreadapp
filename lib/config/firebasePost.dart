90ca4a0195b2166ea2cba8294347a4d1647b86aa
spreadappjl <spreadapp.jl@gmail.com>
2018-10-04 17:16:16 -0300

    doideira

--- lib/config/firebasePost.dart
+++ lib/config/firebasePost.dart
@@ -3,25 +3,19 @@ import 'package:cloud_firestore/cloud_firestore.dart'; // Firebase pluggin
 
 import '../components/post.dart'; //Post Container
 
-
-const bckColor = Color.fromRGBO(30, 30, 30, 1.0);
-
 class PostList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: bckColor,
-      child:StreamBuilder(
-        stream: Firestore.instance.collection('posts').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if(!snapshot.hasData) return Center(child:Text("Loading..."));
-          return ListView(
-            children: snapshot.data.documents.map((DocumentSnapshot document) {
-              return Post(document['title']);
-            }).toList(),
-          );
-        },
-      )
+    return new StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('posts').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return new Center(child:Text('Loading...'));
+        return new ListView(
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return Post(document['title']);
+          }).toList(),
+        );
+      },
     );
   }
 }
